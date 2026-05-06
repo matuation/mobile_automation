5c9043fabfaa2ba7269c451a47159ac50eba81c2
Alexey Schastlivtsev <matuation@gmail.com>
2026-05-06 18:45:59 +0300

    readmefix

--- src/test/java/config/MobileConfig.java
+++ src/test/java/config/MobileConfig.java
@@ -2,49 +2,51 @@ package config;
 
 import org.aeonbits.owner.Config;
 
-    @Config.Sources({
-            "classpath:config/${env}.properties"
-    })
-    public interface MobileConfig extends Config {
-        @Key("platformName")
-        @DefaultValue("Android")
-        String platformName();
+@Config.LoadPolicy(Config.LoadType.FIRST)
+@Config.Sources({
+        "classpath:config/${env}.properties",
+        "classpath:config/local.properties"
+})
+public interface MobileConfig extends Config {
+    @Key("platformName")
+    @DefaultValue("Android")
+    String platformName();
 
-        @Key("deviceName")
-        @DefaultValue("")
-        String deviceName();
+    @Key("deviceName")
+    @DefaultValue("")
+    String deviceName();
 
-        @Key("platformVersion")
-        @DefaultValue("")
-        String platformVersion();
+    @Key("platformVersion")
+    @DefaultValue("")
+    String platformVersion();
 
-        @Key("appPath")
-        @DefaultValue("src/test/resources/apps/app-alpha-universal-release.apk")
-        String appPath();
+    @Key("appPath")
+    @DefaultValue("src/test/resources/apps/app-alpha-universal-release.apk")
+    String appPath();
 
-        @Key("appPackage")
-        @DefaultValue("org.wikipedia.alpha")
-        String appPackage();
+    @Key("appPackage")
+    @DefaultValue("org.wikipedia.alpha")
+    String appPackage();
 
-        @Key("appActivity")
-        @DefaultValue("org.wikipedia.main.MainActivity")
-        String appActivity();
+    @Key("appActivity")
+    @DefaultValue("org.wikipedia.main.MainActivity")
+    String appActivity();
 
-        @Key("isRemote")
-        @DefaultValue("false")
-        boolean isRemote();
+    @Key("isRemote")
+    @DefaultValue("false")
+    boolean isRemote();
 
-        @Key("browserstack.user")
-        @DefaultValue("")
-        String browserstackUser();
+    @Key("browserstack.user")
+    @DefaultValue("")
+    String browserstackUser();
 
-        @Key("browserstack.key")
-        @DefaultValue("")
-        String browserstackKey();
+    @Key("browserstack.key")
+    @DefaultValue("")
+    String browserstackKey();
 
-        @Key("remoteUrl")
-        @DefaultValue("http://185.154.53.106:4444/wd/hub")
-        String remoteUrl();
+    @Key("remoteUrl")
+    @DefaultValue("http://185.154.53.106:4444/wd/hub")
+    String remoteUrl();
 
-    }
+}
 
